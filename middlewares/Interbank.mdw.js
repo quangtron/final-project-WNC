27d8fc07bb3f062a8d501f30ff71ff4af2ec18ad
t2ntruong811 <t2ntruong811@gmail.com>
2020-05-16 21:14:14 +0700

    API Truy vấn thông tin tài khoản
    2 người test lại thử nhé

--- middlewares/Interbank.mdw.js
+++ middlewares/Interbank.mdw.js
@@ -1,13 +1,15 @@
 const createError = require('http-errors');
 const moment = require('moment');
 const bcrypt = require('bcryptjs');
-
+const crypto = require('crypto');
+const cryptoJS = require('crypto-js')
 const config = require('../config/default.json');
 
 module.exports = function(req, res, next) {
     if(req.headers['partner-code'] !== config.interbank.partnerCode){
         throw createError(400, 'Invalid partner code!');
     }
+    console.log(moment().unix());
 
     const ts = moment().unix();
     const headerTs = req.headers['ts'];
@@ -18,9 +20,12 @@ module.exports = function(req, res, next) {
         throw createError(400, 'Request expire!');
     }
 
-    const signature = headerTs + JSON.stringify(req.body);
-    console.log(bcrypt.hashSync(signature));
-    if(!bcrypt.compareSync(signature, req.headers['sign'])){
+    var data = headerTs + JSON.stringify(req.body);
+    var signature = cryptoJS.HmacSHA256(data, config.interbank.secretKey).toString();
+
+    console.log(signature);
+
+    if(signature !== req.headers['sign']){
         throw createError(400, 'Something wrong!');
     }
 
