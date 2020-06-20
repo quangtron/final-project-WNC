35b6a77280fbb9da0859d0476e7f5bce6454f26a
t2ntruong811 <t2ntruong811@gmail.com>
2020-06-21 04:50:57 +0700

    update API debtors (successed)

--- models/debtors.model.js
+++ models/debtors.model.js
@@ -7,6 +7,12 @@ module.exports = {
     all_others_sent_by_card_number: _card_number  => {
         return Debtors.find({card_number: _card_number});
     },
+    all_my_created_unpaid_by_id_customer: _id_customer => {
+        return Debtors.find({id_customer: _id_customer, is_paid: 1});
+    },
+    all_others_sent_unpaid_by_card_number: _card_number  => {
+        return Debtors.find({card_number: _card_number, is_paid: 1});
+    },
     detail: id => {
         return Debtors.findById(id);
     },
@@ -15,5 +21,8 @@ module.exports = {
     },
     del: id => {
         return Debtors.findByIdAndRemove(id);
+    },
+    edit: (condition, entity) => {
+        return Debtors.update(condition, entity);
     }
 }
--- routes/debtors.route.js
+++ routes/debtors.route.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const create_error = require('http-errors');
+const moment = require('moment');
 const low = require('lowdb');
 const fileSync = require('lowdb/adapters/FileSync');
 
@@ -6,33 +8,13 @@ const customers_model = require('../models/customers.model');
 const cards_model = require('../models/cards.model');
 const debtors_model = require('../models/debtors.model');
 const notifications_cancel_debt_model = require('../models/notifications_cancel_debt.model');
+const transactions_model = require('../models/transactions.model');
 const config = require('../config/default.json');
 
 const adapter = new fileSync('./config/default.json');
 const db = low(adapter);
 
 const router = express.Router();
-
-const filter = async (debtors) => {
-    const ret = [];
-
-    const promises = debtors.map(async debtor => {
-        const card_debtor = await cards_model.find_detail_by_card_number(debtor.card_number);
-        const deb = await customers_model.detail(card_debtor.id_customer);
-        const item = {
-            full_name: deb.full_name,
-            card_number: debtor.card_number,
-            money: debtor.money,
-            message: debtor.message
-        }
-        // console.log('item', item);
-        ret.push(item);
-    })
-
-    await Promise.all(promises);
-
-    return ret;
-}
 
 router.get('/my-created', async (req, res) => {
     // ret = { // mình tạo
@@ -102,6 +84,62 @@ router.get('/others-sent', async (req, res) => {
     res.status(200).json(ret);
 })
 
+router.get('/my-created/unpaid', async (req, res) => {
+    const id_customer = req.token_payload.id;
+
+    const debtors = await debtors_model.all_my_created_unpaid_by_id_customer(id_customer);
+    const ret = [];
+
+    const promises = debtors.map(async item => {
+        const card_debtor = await cards_model.find_detail_by_card_number(item.card_number);
+        const debtor = await customers_model.detail(card_debtor.id_customer);
+        const entity_debtor = {
+            _id: item._id,
+            full_name: debtor.full_name,
+            card_number: item.card_number,
+            money: item.money,
+            message: item.message
+        }
+        
+        ret.push(entity_debtor);
+    })
+
+    await Promise.all(promises);
+
+    res.status(200).json(ret);
+
+})
+
+router.get('/others-sent/unpaid', async (req, res) => {
+    const id_customer =  req.token_payload.id;
+    const card = await cards_model.find_payment_card_by_id_customer(id_customer);
+    const card_number = card.card_number;
+    
+    const debtors = await debtors_model.all_others_sent_unpaid_by_card_number(card_number);
+    const ret = [];
+
+    const promises = debtors.map(async item => {
+        const sender = await customers_model.detail(item.id_customer);
+
+        const card_sender = await cards_model.find_payment_card_by_id_customer(sender._id);
+        
+        const entity_sender = {
+            _id: item._id,
+            full_name: sender.full_name,
+            card_number: card_sender.card_number,
+            money: item.money,
+            message: item.message
+        }
+
+        ret.push(entity_sender);
+    })
+
+    await Promise.all(promises);
+    
+    res.status(200).json(ret);
+
+})
+
 router.post('/add', async (req, res) => {
     if(await cards_model.is_exist(req.body.card_number) === false){
         res.status(404).json({errString: 'Number card is not exist!!!'});
@@ -129,10 +167,8 @@ router.post('/delete/:id', async (req, res) => {
     const debtor = await debtors_model.detail(id_debtor);
 
     if(id_customer.toString() === debtor.id_customer.toString()){ // them thong bao huy nhac no do minh tao
-        console.log(id_customer, debtor.id_customer);
-        
         const card_debtor = await cards_model.find_detail_by_card_number(debtor.card_number);
-        console.log(card_debtor);
+        
         const entity_new_notification_cancel_debt = {
             id_customer: card_debtor.id_customer,
             message: req.body.message,
@@ -142,8 +178,6 @@ router.post('/delete/:id', async (req, res) => {
         await notifications_cancel_debt_model.add(entity_new_notification_cancel_debt);
     }  
     else { // them thong bao huy nhac no do nguoi khac gui
-        console.log(id_customer, debtor.id_customer);
-
         const entity_new_notification_cancel_debt = {
             id_customer: debtor.id_customer,
             message: req.body.message,
@@ -154,6 +188,47 @@ router.post('/delete/:id', async (req, res) => {
     }
 
     const ret = await debtors_model.del(id_debtor);
+
+    res.status(200).json(ret);
+})
+
+router.post('/transaction/reminding-debt/:id', async (req, res) => {
+    const id_customer = req.token_payload.id;
+    const id_debtor = req.params.id;
+
+    const debtor = await debtors_model.detail(id_debtor);
+
+    // Vì chỉ thanh toán nhắc nợ những nợ do người khac gửi nên không cần so sánh để kiểm tra cái nào do người khác gửi
+    const card_receiver = await cards_model.find_payment_card_by_id_customer(debtor.id_customer); // card detail cua nguoi se nhan tien (nguoi nhac no)
+    const card_sender = await cards_model.find_payment_card_by_id_customer(id_customer); // card detail cua nguoi tra no 
+    
+    let total_amount = debtor.money + config.account_default.card_maintenance_fee + config.account_default.transaction_fee;
+    
+    if(card_sender.balance < total_amount){
+        throw create_error(400, 'Balance is not enough!');
+    }
+
+    card_sender.balance -= total_amount - config.account_default.card_maintenance_fee;
+    card_receiver.balance += debtor.money;
+
+    await cards_model.edit({_id: card_sender._id}, card_sender);
+    await cards_model.edit({_id: card_receiver._id}, card_receiver);
+
+    debtor.is_paid = 2;
+    await debtors_model.edit({_id: id_debtor}, debtor);
+
+    const entity_new_transaction = {
+        id_customer: id_customer,
+        id_type_transaction: 2,
+        id_partner_bank: 1,
+        card_number: card_receiver.card_number,
+        message: 'Thanh toán nợ',
+        money: debtor.money,
+        type_paid: 1,
+        date_created: moment().format('YYYY-MM-DD HH:mm:ss')
+    }
+
+    const ret = await transactions_model.add(entity_new_transaction);
 
     res.status(200).json(ret);
 })
