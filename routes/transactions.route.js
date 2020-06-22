ea791851610204b47bf7e8e8e11ae7327fab044e
t2ntruong811 <t2ntruong811@gmail.com>
2020-06-22 11:49:08 +0700

    API Transactions (View history of account customer & Recharge into account customer)

--- routes/transactions.route.js
+++ routes/transactions.route.js
@@ -9,6 +9,7 @@ const config = require('../config/default.json');
 
 const router = express.Router();
 
+//customer
 router.post('/customer/sending/add', async (req, res) => {
     const { card_number, money, type_paid } = req.body;
     const id_customer = req.token_payload.id;
@@ -112,6 +113,115 @@ router.get('/customer/reminding-debt', async (req, res) => {
     const id_customer = req.token_payload.id;
 
     const transactions = await transactions_model.all_reminding_debt_by_id_customer(id_customer);
+    const ret = [];
+
+    const promises = transactions.map(async item => {
+        const card_receiver = await cards_model.find_detail_by_card_number(item.card_number);
+        const receiver = await customers_model.detail(card_receiver.id_customer);
+        
+        const entity_ret_item = {
+            _id: item._id,
+            full_name: receiver.full_name,
+            card_number: item.card_number,
+            money: item.money,
+            message: item.message,
+            date_created: item.date_created
+        }
+
+        ret.push(entity_ret_item);
+    })
+
+    await Promise.all(promises);
+
+    res.status(200).json(ret);
+
+})
+
+//Teller
+router.post('/teller/sending/add', async (req, res) => {
+    const { card_number, money } = req.body;
+    // const id_customer = req.token_payload.id;
+
+    if(await cards_model.is_exist(card_number) === false){
+        throw create_error(400, 'Number card is not exist!');
+    }
+
+    const card_receiver = await cards_model.find_detail_by_card_number(card_number);
+    card_receiver.balance += money;
+
+    await cards_model.edit({_id: card_receiver._id}, card_receiver);
+
+    // await transactions_model.add({
+    //     ...req.body,
+    //     id_customer,
+    //     id_type_transaction: 1,
+    //     id_partner_bank: 1,
+    //     date_created: moment().format('YYYY-MM-DD HH:mm:ss')
+    // });
+
+    res.status(200).json(req.body);
+})
+
+router.get('/teller/receiving', async (req, res) => {
+    const {card_number} = req.body;
+
+    const transactions = await transactions_model.all_receiving_by_card_number(card_number);
+    const ret = [];
+
+    const promises = transactions.map(async item => {
+        const sender = await customers_model.detail(item.id_customer);
+        const card_sender = await cards_model.find_payment_card_by_id_customer(sender._id);
+
+        const entity_ret_item = {
+            _id: item._id,
+            full_name: sender.full_name,
+            card_number: card_sender.card_number,
+            money: item.money,
+            message: item.message,
+            date_created: item.date_created
+        }
+
+        ret.push(entity_ret_item);
+    })
+    
+    await Promise.all(promises);
+
+    res.status(200).json(ret);
+})
+
+router.get('/teller/sending', async (req, res) => {
+    const {card_number} = req.body;
+    const card_customer = await cards_model.find_detail_by_card_number(card_number);
+
+    const transactions = await transactions_model.all_sending_by_id_customer(card_customer.id_customer);
+    const ret = [];
+
+    const promises = transactions.map(async item => {
+        const card_receiver = await cards_model.find_detail_by_card_number(item.card_number);
+        const receiver = await customers_model.detail(card_receiver.id_customer);
+        
+        const entity_ret_item = {
+            _id: item._id,
+            full_name: receiver.full_name,
+            card_number: item.card_number,
+            money: item.money,
+            message: item.message,
+            date_created: item.date_created
+        }
+
+        ret.push(entity_ret_item);
+    })
+
+    await Promise.all(promises);
+
+    res.status(200).json(ret);
+})
+
+router.get('/teller/reminding-debt', async (req, res) => {
+    const {card_number} = req.body;
+    const card_customer = await cards_model.find_detail_by_card_number(card_number);
+    console.log(card_customer.id_customer);
+    const transactions = await transactions_model.all_reminding_debt_by_id_customer(card_customer.id_customer);
     const ret = [];
 
     const promises = transactions.map(async item => {
